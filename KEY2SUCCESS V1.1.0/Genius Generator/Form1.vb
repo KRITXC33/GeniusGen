50f49d607492209bb8060651a0b8a178f15984f2
silence <79935756+SILENCExKritic@users.noreply.github.com>
2022-03-07 17:15:35 -0800

    Add files via upload

---
+++ KEY2SUCCESS V1.1.0/Genius Generator/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.Click
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("You have successfully become a genius!")
+    End Sub
+
+
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        MsgBox("What is the sum 10 + -7?")
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        MsgBox("Genius successfully deactivated!")
+    End Sub
+
+End Class
+#Disable Warning
